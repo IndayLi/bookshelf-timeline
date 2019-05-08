d56b790ecace725338dc4ae67179ce2354493a31
Leona Marzinotto <LeonaMerk@gmail.com>
2019-05-08 10:47:50 -0400

    Update: Added columns to user table, updated model test, updated registration form. Passing tests, and ready for PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/i }
+  validates :password, presence: true, length: { minimum: 10 }, on: :create
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
+DatabaseCleaner.clean_with(:truncation)
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "tests User model" do
+    user_1 = User.create(first_name: 'first1', email: 'email_1@test.com', password: 'password123',  password_confirmation: 'password123', image_url: 'https://www.nicepng.com/maxp/u2w7r5y3a9q8e6y3/')
+    user_2 = User.create(first_name: 'first2', email: 'email_2@test.com', password: 'password123',  password_confirmation: 'password123')
+    user_3 = User.create(email: 'email_3@test.com', password: 'password123',  password_confirmation: 'password123', image_url: 'https://www.nicepng.com/maxp/u2w7r5y3a9q8e6y3/')
+    user_4 = User.create(first_name: 'first4', email: 'email_2@test.com', password: 'password123',  password_confirmation: 'password123')
+    user_5 = User.create(first_name: 'first5', email: 'email_5@test.com', password: 'password',  password_confirmation: 'password')
+
+    it "successfully creates user" do
+      expect(user_1.valid?).to eq(true)
+      expect(user_2.valid?).to eq(true)
+    end
+
+    it "fails to create user" do
+      expect(user_3.valid?).to eq(false)
+      expect(user_4.valid?).to eq(false)
+      expect(user_5.valid?).to eq(false)
+    end
+  end
 end
