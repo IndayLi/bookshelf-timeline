28ad7fa2865a841ddf46e29ba9a00744d389c00e
Leona Marzinotto <LeonaMerk@gmail.com>
2019-05-08 11:49:15 -0400

    fixed type

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   root "homes#index"
-
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
